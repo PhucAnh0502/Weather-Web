299815038f08b2d95e70f3e1ff95525bed177431
Nguyen Phuc Anh <phucanh05022004@gmail.com>
2025-01-02 00:09:59 +0700

    add ForecastWeather.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import './components/ForecastWeather'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>Hello World</div>
+      <CurrentWeather />
     </>
   )
 }
---
+++ src/components/ForecastWeather.jsx
@@ -0,0 +1,13 @@
+function CurrentWeather(){
+    return (
+        <>
+            <div className="CurrentDTM">dtm</div>
+            <div className="Temperature">
+                <img></img>
+                <p>Temp C</p>
+            </div>
+        </>
+    )
+}
+
+export default CurrentWeather;
